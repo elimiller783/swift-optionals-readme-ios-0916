3d1bffaf187a7b6568eeeb05a0e92dc50290d43f
elimiller783 <emiller783@gmail.com>
2016-09-05 19:57:57 -0400

    var done: String?

--- Pets.playground/Contents.swift
+++ Pets.playground/Contents.swift
@@ -16,3 +16,22 @@ if let friendsPet = petName {
     print("My friend's pet is \(friendsPet)")
     print("petName is still an Optional! \(petName)")
 }
+
+var bday: Int?
+bday = 923
+print(bday)
+
+if bday != nil{
+    print("Happy bday its \(bday)")
+} else {
+    print("It aint your bday")
+}
+
+if let bday = bday {
+    print("Happy bday its \(bday)")
+}
+
+if let mybday = bday {
+    print("\(mybday)")
+    print("\(bday)")
+}
